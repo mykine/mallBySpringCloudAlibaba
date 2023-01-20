575046d7bf21244f7a4865b00f51d26a2f811b7c
jy@mykine <mykine@sina.com>
2023-01-20 14:03:46 +0800

    添加应用名config

---
+++ mall-order/src/main/java/cn/mykine/mall/order/config/ProjectNameConfig.java
@@ -0,0 +1,21 @@
+package cn.mykine.mall.order.config;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class ProjectNameConfig implements EnvironmentAware {
+
+    @Value("${spring.application.name}")
+    private  String applicationName;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        if(StringUtils.isBlank(System.getProperty("project.name"))){
+            System.setProperty("project.name",applicationName);
+        }
+    }
+}
